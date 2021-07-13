5673484c4081548147f5d26c284080ddb8420963
taylorsuccessor <taylorsuccessor@users.noreply.github.com>
2021-07-13 15:53:45 +0300

    Update setup_vim_on_pod.sh

--- setup_vim_on_pod.sh
+++ setup_vim_on_pod.sh
@@ -1,3 +1,16 @@
+ useradd -m -s $(which bash) -G sudo posrocket 
+echo -e "pos123456\npos123456" | passwd posrocket 
+echo 'posrocket ALL=NOPASSWD: ALL' >> /etc/sudoers 
+
+
 apt install vim -y
+apt-get install acl
+#setfacl -R -m u:posrocket:w /usr/src
+#chmod -R 0777 /usr/src/app
+chown -R posrocket:posrocket /usr/src
+su - posrocket
 wget https://taylorsuccessor.github.io/vim_fast_setup.sh
 sh vim_fast_setup.sh
+cd /usr/src/app
+vim 
+
