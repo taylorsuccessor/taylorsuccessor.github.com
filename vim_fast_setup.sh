8981faf3700f2ccccf35cde25494634ceea28343
taylorsuccessor <taylorsuccessor@users.noreply.github.com>
2021-07-13 20:39:43 +0300

    Update vim_fast_setup.sh

--- vim_fast_setup.sh
+++ vim_fast_setup.sh
@@ -1,6 +1,6 @@
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ets-labs/python-vimrc/master/setup.sh)"
 
-curl https://taylorsuccessor.github.io/vimrc ~/.vimrc
+curl https://taylorsuccessor.github.io/vimrc -o ~/.vimrc
 
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/plugin/Vundle.vim
 mkdir ~/.vim/undodir
